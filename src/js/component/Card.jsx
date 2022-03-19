a95b24e14d8d2ee3f1771defe2d7e0f384404d68
Aleco112 <aleco112@yahoo.fr>
2022-03-19 00:59:24 +0000

    new-one

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -3,18 +3,11 @@ import React from "react";
 export function Card(props) {
 	return (
 		<>
-			<div className="card" style={{ width: "18rem;" }}>
-				<img src="" className="card-img-top" alt="..." />
+			<div className="card" style={{ width: "18rem" }}>
+				<img src={props.image} />
 				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
-					<p className="card-text">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-						Vestibulum rutrum ac neque quis dictum. Ut nec tellus
-						tempus, finibus sapien mattis, feugiat sem. Aenean a
-						pellentesque sapien. Suspendisse ligula nibh, efficitur
-						eget turpis rhoncus, volutpat tincidunt nisi. Proin
-						facilisis erat placerat sodales lacinia.
-					</p>
+					<h5 className="card-title">{props.title}</h5>
+					<p className="card-text">{props.text} </p>
 					<a href="#" className="btn btn-primary">
 						Find out more
 					</a>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,11 +6,17 @@ import { Jumbotron } from "./Jumbotron.jsx";
 const Home = () => {
 	return (
 		<div>
-			<Jumbotron />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
+			<nav>
+				<Jumbotron />
+				<Card
+					title="Car"
+					image="https://picsum.photos/200"
+					text="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content"
+				/>
+				<Card />
+				<Card />
+				<Card />
+			</nav>
 		</div>
 	);
 };
